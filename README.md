0400fe99765d0235cdd716ad2ec3207052c9d62b
neco <josehenriqueneco@gmail.com>
2020-05-05 19:20:22 -0300

    finalizado o salvamento automático das imagens
    05 de maior de 2020

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-## Automatização de download de imagens de ebooks (Em estudo)
-Download de imagens de ebooks do cengagebrasil / vitalsource com o framework [selenium](https://selenium-python.readthedocs.io/) e python
+## Download de imagens de páginas de ebook
+Automatize o download de imagens de ebooks do cengagebrasil/vitalsource com o framework [selenium](https://selenium-python.readthedocs.io/) e python
 
-- [x] Login e urls das páginas individuais
-- [ ] Requisição de url(src) como imagens de páginas
-- [ ] Download de todas as imagens
+- - -
+### O QUE FAZ?
+- [x] Login e download de imagns
 - [ ] Converter imagens em arquivo(.pdf)
 
-Sinta-se livre para contribuir!
+### COMO USAR?
+Instale o selenium e faça o download do chromedriver(chrome) ou geckodriver(firefox - necessário alteração no código)
+
+#### INSTALAR O SELENIUM
+```python
+pip install -U selenium
+```
+
+#### VERIFICAR NÚMERO ISBN DO LIVRO
+Existem duas formas:
+1. Através do cengagebrasil.vitalsource.com/#/books/`9788522124015`
+2. Por meio da página do livro OU internet:
+![1](img/isbn.jpg)
+
+#### NÚMERO DA PÁGINA INICIAL
+Abra o ebook e com auxilio do navegador inspecione a página:
+![2](img/page.jpg)
+
+*Veja que ali também é possível visualizar o ISBN*
+
+
+- - -
+*Desenvolvido com o intuito de estudo, recomendamos que não comercialize ou matenha cópias não autorizadas de livros*
+
+*Sinta-se livre para contribuir!*
--- app.py
+++ app.py
@@ -1,62 +1,111 @@
 import time
-# from PIL import Image
-# import img2pdf
+
+from PIL import Image
+from io import BytesIO
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 
-def login_and_save_urls(email, senha, isbn, nome_livro, pagina_inicial, total_paginas):
-    browser = webdriver.Firefox()
-    
-    # login
+def url_paginas(isbn, pagina_inicial, pagina_final):
+    paginas = []
+    book = f"https://jigsaw.vitalsource.com/books/{isbn}"
+    for page in range(pagina_inicial, pagina_final):
+        url = f"{book}/pages/{page}"
+        paginas.append(url)
+
+    return paginas
+
+
+def login(email, senha, driver):
     url = 'https://cengagebrasil.vitalsource.com/#/user/signin'
 
-    if url == 'https://cengagebrasil.vitalsource.com/#/user/signin':
-        browser.get(url)
+    driver.get(url)
 
-        username = browser.find_element_by_id("email-field")
-        password = browser.find_element_by_id("password-field")
-
-        time.sleep(5)
-        username.send_keys(email)
-        password.send_keys(senha)
-
-        time.sleep(5)
-        btn_login = browser.find_element_by_xpath("//*[@class='Button__buttonContent-bIKrMk KaEKF']")
-        btn_login.submit()
-
-    # abrir páginas e salvar urls da imagem
     time.sleep(3)
-    browser.execute_script("window.open()")
-    browser.switch_to.window(browser.window_handles[1])
+    username = driver.find_element_by_id("email-field")
+    password = driver.find_element_by_id("password-field")
 
-    src_img = []
-    for page in range(pagina_inicial, total_paginas+1):
+    time.sleep(5)
+    username.send_keys(email)
+    password.send_keys(senha)
 
-        url = 'https://jigsaw.vitalsource.com/books/'+str(isbn)+'/pages/'+str(page)
-        browser.get(url)
+    time.sleep(3)
+    btn_login = driver.find_element_by_xpath("//*[@class='Button__buttonContent-bIKrMk KaEKF']")
+    btn_login.submit()
+    time.sleep(5)
 
-        # NÃO ESTÁ RETORNANDO SRC
-        time.sleep(2)
-        element_src = browser.find_element_by_xpath("html//body//div[@id='epub-container']//iframe[@id='epub-content']//html//body[@class='pbk']//img[@id='pbk-page']")
-        print(element_src)
 
-        src_img.append(element_src)
+def save_screenshot(driver, largura, altura, file_name):
+    driver.set_window_size(largura, altura)
+    img_binary = driver.get_screenshot_as_png()
+    img = Image.open(BytesIO(img_binary))
+    img.save(file_name)
 
-    # funcao download imagens
-    return src_img
+    print(f"{file_name} salva!")
+
+
+def open_url(email, senha, lista_urls_paginas, nome_livro, largura, altura):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(chrome_options=options)
+
+    driver.maximize_window()
+    login(email, senha, driver)
+    
+    for page in range(0, len(lista_urls_paginas)):
+        driver.get(lista_urls_paginas[page])
+        time.sleep(6)
+        save_screenshot(driver, largura, altura, f'{nome_livro}_{page}.png', ) 
+
 
 def main():
-    email = str(input('Email: '))
-    senha = str(input('Senha: '))
-    isbn = int(input('ISBN: '))
-    nome_livro = str(input('Nome do livro: '))  # criar pasta para salvar imagem com este nome
-    pagina_inicial = 351539662  # fin.element_by ...
-    total_paginas = 10
-    total_paginas = (pagina_inicial + total_paginas)
+    #  Entradas de dados:
+    print("\nDOWNLOAD LIVROS DA BIBLIOTECA VITALSOURCE / CENGAGEBRASIL")
+    print("Antes de tudo, leia o README para não ocorrer erros!\n")
+    continuar = str(input('Continuar? sim/não\n')).upper()
 
-    print(login_and_save_urls(email, senha, isbn, nome_livro, pagina_inicial, total_paginas))
+    if continuar == "SIM":
+        email = str(input("Email: "))
+        senha = str(input("Senha: "))
+        isbn = str(input("Qual o ISBN do livro: "))
+        nome_livro = str(input("Identifique o livro(ex: algebra): "))
+        pagina_inicial = int(input("Qual o número da página inicial, leia o README para maais detalhes: "))
+
+        print(
+            "\nRECOMENDO TESTAR AS RESOLUÇÕES ANTES DE IMPRIMIR TODAS AS PÁGINAS!\n"
+            "PORTANTO, INSIRA UMA QUANTIDADE MENOR QUE 15 PARA TESTES\n")
+        total_paginas = int(input('Quantidade de páginas: '))
+        pagina_final = (pagina_inicial + total_paginas)
+
+        print("\nAS PÁGINAS TEM RESOLUÇÕES DIFERENTES A DEPENDER DO LIVRO, REALIZE TESTES OU VERIFIQUE PELO HTML")
+        print(
+            "RESOLUÇÕES:\n\n"
+            "[1] 800 x 1065\n"
+            "[2] 1600 x 2131\n"
+            "[3] 2000 x 2664\n"
+            "[4] 2000 x 2888\n")
+
+        res = int(input("Quais resolução das páginas? "))
+
+        largura, altura = 0, 0
+        if res == 1:
+            largura, altura = 800, 1065
+        elif res == 2:
+            largura, altura = 1600, 2131
+        elif res == 3:
+            largura, altura = 2000, 2664
+        elif res == 4:
+            largura, altura = 2000, 2888
+
+        
+        lista_urls_paginas = url_paginas(isbn, pagina_inicial, pagina_final)
+
+        open_url(email, senha, lista_urls_paginas, nome_livro, largura, altura)
+    
+    else:
+        print('LEIA O README!')
+    
 
 
 main()
