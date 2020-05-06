e84f1fabb9e462971a991da31a36345cf563e410
neco <josehenriqueneco@gmail.com>
2020-05-06 13:12:39 -0300

    firefox - descricao

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 ## Download de imagens de páginas de ebook
-Automatize o download de imagens de ebooks do cengagebrasil/vitalsource com o framework [selenium](https://selenium-python.readthedocs.io/) e python
-
-*Desenvolvido com o intuito de estudo, recomendamos que não comercialize ou matenha cópias não autorizadas de livros*
+Automatiza o screenshot de páginas de ebooks da biblioteca virtual cengagebrasil/vitalsource com __[selenium](https://selenium-python.readthedocs.io/)__ e __python__
+<br>
+*Desenvolvido com intuito de estudo de web-scraping, recomendo que não comercialize ou matenha cópias não autorizadas de livros*
 
 - - -
-### O QUE FAZ?
-- [x] Login e download de imagns
-- [ ] Converter imagens em arquivo(.pdf)
-
-### COMO USAR?
-Instale o selenium e faça o download do chromedriver(chrome) ou geckodriver(firefox - necessário alteração no código)
+### Como usar?
+1. Instalar Selenium
+2. Número ISBN do livro
+3. Número da página inicial
 
 #### INSTALAR O SELENIUM
+Faça o download do __chromedriver__(chrome) ou __geckodriver__(firefox - necessário pequenas alterações no código)
+
+*Instalando o Selenium*
 ```python
 pip install -U selenium
 ```
@@ -28,13 +29,13 @@ Abra o ebook e com auxilio do navegador inspecione a página e encontre o iframe
 
 ![2](img/page.jpg)
 
-*Veja que ali também é possível visualizar o ISBN*
-
+> *Veja que ali também é possível visualizar o ISBN*
+---
 ### Utilize o próprio recurso(Imprimir) do Windows para transformar as imagens em PDF
 Selecione todas as imagens, clique na primeira(capa) imagem com o botão direito e Imprimir, configure e salve como pdf.
 
 ### Salvando várias vezes a mesma página(capa/contracapa/etc)
-Recomendo colocar um número maior de páginas, o dobro, e verificar com um tempo se já baixou a ultima página.
+Recomendo colocar um número maior de páginas, após um período verificar se já baixou a ultima página. Isto demonstra estar perto do limite de acessos, e provavelmente aparecerá captcha, tornando as screenshots páginas em branco. Tente novamente depois de algumas horas, ou __baixe manualmente, inspecionando elementos e salvando fácilmente a página__.
 
 - - -
 
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
 
 
 def url_paginas(isbn, pagina_inicial, pagina_final):
@@ -17,12 +18,12 @@ def url_paginas(isbn, pagina_inicial, pagina_final):
     return paginas
 
 
-def login(email, senha, driver):
-    url = 'https://cengagebrasil.vitalsource.com/#/user/signin'
+def login(driver, email, senha, isbn):
+    url = f"https://cengagebrasil.vitalsource.com/#/books/{isbn}"
 
     driver.get(url)
 
-    time.sleep(3)
+    time.sleep(8)
     username = driver.find_element_by_id("email-field")
     password = driver.find_element_by_id("password-field")
 
@@ -45,49 +46,53 @@ def save_screenshot(driver, largura, altura, file_name):
     print(f"{file_name} salva!")
 
 
-def open_url(email, senha, lista_urls_paginas, nome_livro, largura, altura):
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(chrome_options=options)
+def open_url(email, senha, lista_urls_paginas, isbn, nome_livro, largura, altura):
+    option = Options()
+    option.headless = True
+    driver = webdriver.Chrome(chrome_options=option)
+    # driver = webdriver.Firefox(options=option)
 
     driver.maximize_window()
-    login(email, senha, driver)
+    login(driver, email, senha, isbn)
     
     for page in range(0, len(lista_urls_paginas)):
         driver.get(lista_urls_paginas[page])
         time.sleep(6)
-        save_screenshot(driver, largura, altura, f'{nome_livro}_{page}.png', ) 
+        save_screenshot(driver, largura, altura, f'{nome_livro}_{page}.png') 
 
 
 def main():
     #  Entradas de dados:
-    print("\nDOWNLOAD LIVROS DA BIBLIOTECA VITALSOURCE / CENGAGEBRASIL")
+    print("\nDOWNLOAD DE IMAGENS DE PÁGINAS DA BIBLIOTECA VITALSOURCE/CENGAGEBRASIL")
     print("Antes de tudo, leia o README para não ocorrer erros!\n")
-    continuar = str(input('Continuar? sim/não\n')).upper()
+    print("- Desenvolvido com intuito de estudo de web-scraping, recomendo que não comercialize ou matenha cópias não autorizadas de livros")
+    print("-> Foi verificado um limite de acessos, após este limite é necessário preencher um captcha(ainda não consegui quebralo(pois é do Google xD)\n")
+    
+    continuar = str(input('Continuar? (sim/não)\n')).upper()
 
     if continuar == "SIM":
+        print("\n-> INFORMAÇÕES <-")
         email = str(input("Email: "))
         senha = str(input("Senha: "))
         isbn = str(input("Qual o ISBN do livro: "))
         nome_livro = str(input("Identifique o livro(ex: algebra): "))
-        pagina_inicial = int(input("Qual o número da página inicial, leia o README para maais detalhes: "))
+        pagina_inicial = int(input("Qual o número da página inicial, leia o README para mais detalhes: "))
 
         print(
             "\nRECOMENDO TESTAR AS RESOLUÇÕES ANTES DE IMPRIMIR TODAS AS PÁGINAS!\n"
-            "PORTANTO, INSIRA UMA QUANTIDADE MENOR QUE 15 PARA TESTES\n")
+            "PORTANTO, INSIRA UMA QUANTIDADE MENOR QUE 15 PARA TESTES")
         total_paginas = int(input('Quantidade de páginas: '))
         pagina_final = (pagina_inicial + total_paginas)
 
         print("\nAS PÁGINAS TEM RESOLUÇÕES DIFERENTES A DEPENDER DO LIVRO, REALIZE TESTES OU VERIFIQUE PELO HTML")
         print(
-            "RESOLUÇÕES:\n"
+            "-> RESOLUÇÕES <-:\n"
             "[1] 800 x 1065\n"
             "[2] 1600 x 2131\n"
             "[3] 2000 x 2664\n"
             "[4] 2000 x 2888\n")
 
-        print("Recomenamos a opção [4]")
-        res = int(input("Qual resolução deseja?\nRecomendo a [4]"))
+        res = int(input("Qual resolução deseja? (Recomendo a [4]) "))
 
         largura, altura = 0, 0
         if res == 1:
@@ -102,11 +107,10 @@ def main():
         
         lista_urls_paginas = url_paginas(isbn, pagina_inicial, pagina_final)
 
-        open_url(email, senha, lista_urls_paginas, nome_livro, largura, altura)
+        open_url(email, senha, lista_urls_paginas, isbn, nome_livro, largura, altura)
     
     else:
         print('LEIA O README!')
     
-
 
 main()
